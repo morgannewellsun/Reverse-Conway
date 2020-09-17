40518826fb8ab72d7b1b87710a5b7eff578951ee
mathtester <sunheng@hotmail.com>
2020-09-17 00:08:04 -0400

    minor enhancements on data/result viewing

--- method_1/kaggle_data.py
+++ method_1/kaggle_data.py
@@ -4,13 +4,16 @@
 import csv
 import logging
 from gamelib.gamelife import GameLife
-from gamelib.logutil import init_game_log, kaggle_root
+from gamelib.logutil import init_game_log, kaggle_root, output_root
 
 
 init_game_log('Kaggle data check')
 side_len = 25
 board_size = side_len * side_len
 
+# Count statistics: key is tuple (delta, start_count, end_count)
+# value is the number of occurences.
+count_stats = dict()
 matched = True
 still_cnt = 0
 with open(kaggle_root + 'train.csv', newline='') as csvfile:
@@ -23,6 +26,8 @@ with open(kaggle_root + 'train.csv', newline='') as csvfile:
         s_brd = [int(j) for j in start_arr]
         stop_arr = row[(board_size+2):(2*board_size+2)]
         k_brd = [int(j) for j in stop_arr]
+        key = (delta, sum(s_brd), sum(k_brd))
+        count_stats[key] = count_stats.setdefault(key, 0) + 1
         
         gl = GameLife(side_len, side_len)
         still_cnt += gl.run(start_array = s_brd, iterations = int(delta))
@@ -40,3 +45,9 @@ if matched:
     msg = 'All {} records are matched with {} still.'.format(game_idx+1, still_cnt)
     print(msg)
     logging.info(msg)
+
+with open(output_root + 'nn_predict/count_stats.csv', 'w+') as fh:
+    fh.write('delta,start,stop,count\n')
+    for k, v in count_stats.items():
+        fh.write('{},{},{},{}\n'.format(k[0], k[1], k[2], v))
+        
